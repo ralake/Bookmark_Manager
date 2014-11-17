bdd616fff255f9a0b3079ddcfd8245fefd4f0ddb
Richard Lake <ralake1985@gmail.com>
2014-11-17 11:08:19 +0000

    Add file structure and link class

---
+++ lib/link.rb
@@ -0,0 +1,13 @@
+class Link
+  # Class corresponds to a table in the databse
+  # Can use this to maniuplate the data
+
+  # this line makes instances of the link class Datamapper resources
+  include DataMapper::Resource
+
+  # This block describes what resources our model will have
+  property :id,    Serial # Serial means that it will be auto-incremented for every record
+  property :title, String
+  property :url,   String
+
+end
