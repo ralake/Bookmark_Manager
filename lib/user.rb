28b78acd707fcb09d8664ff34f925176efc4b94c
Richard Lake <ralake1985@gmail.com>
2014-11-19 11:51:18 +0000

    Add authenticate class method to User model

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
 require 'bcrypt'
+
 class User
 
   include DataMapper::Resource
@@ -16,6 +17,15 @@ class User
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = first(:email => email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
   end
 
 end
