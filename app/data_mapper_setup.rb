ea25b8667fb1841b4537fcf1ebdcddce291ec8dd
Richard Lake <ralake1985@gmail.com>
2014-11-21 10:45:45 +0000

    minor change

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,11 +1,5 @@
 env = ENV["RACK_ENV"] || "development"
 
-database_url = if env == 'production'
-    ENV['DATABASE_URL']
-  else
-    ENV['DATABASE_URL'] + "_#{env}"
-  end
-
 DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/bookmark_manager_#{env}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
