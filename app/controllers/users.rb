2715a69a97632d1b940d40c0af13e0224fdc61d0
Richard Lake <ralake1985@gmail.com>
2014-11-21 12:54:32 +0000

    minor change

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -33,7 +33,7 @@ class BookmarkManager
                       # :to      => "#{@email}",
                       :to      => 'ralake1985@gmail.com',
                       :subject => 'Reset Password',
-                      :text    => "Please follow the following link to reset your password.\n http://localhost:9292/users/reset_password/#{@token}"}
+                      :text    => "Please follow the following link to reset your password.\n http://http://vast-gorge-8099.herokuapp.com//users/reset_password/#{@token}"}
     mg_client.send_message "sandbox9533715787bf49408a576e6f77424cf3.mailgun.org", message_params
     flash[:notice] = "Please check your email"
     redirect '/'
