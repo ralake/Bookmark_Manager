a56054919130b288d1445fb901e28e017e226e9d
Richard Lake <ralake1985@gmail.com>
2014-11-21 10:41:21 +0000

    configure for heroku deploy

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,12 @@
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+database_url = if env == 'production'
+    ENV['DATABASE_URL']
+  else
+    ENV['DATABASE_URL'] + "_#{env}"
+  end
+
+DataMapper.setup(:default, ENV["DATABASE_URL" || "postgres://localhost/bookmark_manager_#{env}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
